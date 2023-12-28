49aa5a15e48ba082693db64891b75b993d51692f
mordecai-x <105885497+mordecai-x@users.noreply.github.com>
2023-12-28 16:50:27 -0500

    exceeds expectations
    update html to add input element for a search bar
    update css to style and place the search bar
    update css to display and place 'prev' and 'next' buttons
    add functionality to the search bar to filter employees by name
    add functionality to the modal overlay to switch between employees using the 'prev' and 'next' buttons

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function displayEmployees(employeeData) {
                 <img class="avatar" src="${picture.large}" alt>
                 <div class="text-container">
                     <h2 class="name">${name.first} ${name.last}</h2>
-                    <p class="email">d${email}</p>
+                    <p class="email">${email}</p>
                     <p class="address">${city}</p>
                 </div>
             </div>
@@ -48,7 +48,7 @@ function displayModal(index) {
     let date = new Date(dob.date);
     const modalHTML = `
         <img class="avatar" src="${picture.large}" alt>
-        <div class="text-container">
+        <div class="text-container" data-index="${index}">
             <h2 class="name">${name.first} ${name.last}</h2>
             <p class="email">${email}</p>
             <p class="address">${city}</p>
@@ -76,4 +76,45 @@ gridContainer.addEventListener('click', e => {
 
 modalClose.addEventListener('click', e => {
     overlay.classList.add('hidden');
+});
+
+// ------------------------------------------
+// search bar function
+// ------------------------------------------
+const searchBox = document.getElementById('user-field');
+
+searchBox.onkeyup = function() {
+    let input = searchBox.value;
+    input = input.toLowerCase();
+    let cards = document.querySelectorAll('.card');
+    for (let i = 0; i < cards.length; i++) {
+        if (!cards[i].childNodes[3].childNodes[1].innerHTML.toLowerCase().includes(input)) {
+            cards[i].style.display = 'none';
+        } else {
+            cards[i].style.display = 'flex';
+        }
+    }
+}
+
+// ------------------------------------------
+// modal lightbox controls
+// ------------------------------------------
+const next = document.querySelector('.next');
+const previous = document.querySelector('.prev');
+
+next.addEventListener('click', (e) => {
+    let index = e.target.previousElementSibling.previousElementSibling.childNodes[3].getAttribute('data-index');
+    index = +index;
+    if (index >= 11) {
+        index = -1 // because index will increase by 1 when 'displayModal' runs and index starts at 0
+    }
+    displayModal(index + 1); // index increases by 1 when function runs
+});
+previous.addEventListener('click', (e) => {
+    let index = e.target.previousElementSibling.childNodes[3].getAttribute('data-index');
+    index = +index;
+    if (index < 1) {
+        index = 12
+    }
+    displayModal(index - 1);
 });
